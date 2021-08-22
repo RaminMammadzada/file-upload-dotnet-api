2bc532ba28d7cdea35f8304d99b4f8d5c10ac7d3
Ramin Mammadzada <remki-design@hotmail.com>
2021-08-22 21:20:27 +0400

    Return data as well when file is created.

--- UploadFilesServer/Controllers/FileItemsController.cs
+++ UploadFilesServer/Controllers/FileItemsController.cs
@@ -8,58 +8,58 @@ using UploadFilesServer.Entities;
 
 namespace UploadFilesServer.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FileItemsController : ControllerBase
-    {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class FileItemsController : ControllerBase
+  {
 
     private readonly DataContext _context;
 
-        public FileItemsController(DataContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public IActionResult GetAllFiles()
-        {
-            try
-            {
-                var files = _context.FileItems.ToList();
-
-                return Ok(files);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-        }
-
-        [HttpPost]
-        public IActionResult CreateFile([FromBody]FileItem fileItem)
-        {
-            try
-            {
-                if (fileItem == null)
-                {
-                    return BadRequest("FileItem object is null");
-                }
-
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest("Invalid model object");
-                }
-
-                fileItem.Id = Guid.NewGuid();
-                _context.Add(fileItem);
-                _context.SaveChanges();
-
-                return StatusCode(201);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex}");
-            }
-        }
+    public FileItemsController(DataContext context)
+    {
+      _context = context;
     }
+
+    [HttpGet]
+    public IActionResult GetAllFiles()
+    {
+      try
+      {
+        var files = _context.FileItems.ToList();
+
+        return Ok(files);
+      }
+      catch (Exception ex)
+      {
+        return StatusCode(500, $"Internal server error: {ex}");
+      }
+    }
+
+    [HttpPost]
+    public IActionResult CreateFile([FromBody] FileItem fileItem)
+    {
+      try
+      {
+        if (fileItem == null)
+        {
+          return BadRequest("FileItem object is null");
+        }
+
+        if (!ModelState.IsValid)
+        {
+          return BadRequest("Invalid model object");
+        }
+
+        fileItem.Id = Guid.NewGuid();
+        _context.Add(fileItem);
+        _context.SaveChanges();
+
+        return StatusCode(201, fileItem);
+      }
+      catch (Exception ex)
+      {
+        return StatusCode(500, $"Internal server error: {ex}");
+      }
+    }
+  }
 }
